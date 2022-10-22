747f3d33d6aab6d3c705a1659b3216018d773ce8
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-10-21 21:11:13 -0500

    convertidor de archivos terminado

--- converter/views/views.py
+++ converter/views/views.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import re
 from nis import match
-
+from pydub import AudioSegment
 import jwt
 from celery import Celery
 from flask import redirect, request, url_for
@@ -10,7 +10,7 @@ from flask_jwt_extended import create_access_token, jwt_required
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
-
+import sys
 from ..models import (AudioFormat, File, FileSchema, ProcessStatus, Task,
                       TaskSchema, User, UserSchema, db)
 
@@ -24,11 +24,32 @@ ALLOWED_EXTENSIONS = {'mp3', 'ogg', 'wav'}
 
 class ViewConverter(Resource):
 
-    @jwt_required()
+    
     def post(self):
-        response= {"mensaje": "Se ha convertido el archivo con exito","error":False}
-        q='qq'
-        return q
+        try:
+            
+
+            user = User.query.get_or_404(request.json["user_id"])
+            task = Task.query.get_or_404(request.json["task_id"])
+            file = File.query.get_or_404(request.json["input_file_id"])
+            output_extention = request.json["output_extention"]
+            new_path='files/'+datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S_%f")+'.'+output_extention 
+            input_file_path=file.path
+            input_file_format=file.path.split(".")[1]
+
+            from_file = AudioSegment.from_file(input_file_path, input_file_format)
+            from_file.export(new_path, format=output_extention)
+            
+            new_file=File(name=file.name.split(".")[0]+"."+output_extention,extention=getExtention(output_extention),path=new_path,timestamp = datetime.datetime.now(),user_id=user.id)
+            db.session.add(new_file)    
+            db.session.commit() 
+
+            task.output_file_id=new_file.id
+            task.status=ProcessStatus.PROCESSED
+            db.session.commit()  
+            return  {"mensaje": "Se ha convertido el archivo con exito","error":False}
+        except:
+            return {"mensaje": "Hubo un error no esperado","error":True}
         
 
        
