9edc7f8b46f2ba593e2cd016461d4fff805b7f6e
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-10-26 20:39:01 -0500

    postgres user

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ from flask_jwt_extended import JWTManager
 def create_app(config_name):
         app = Flask(__name__)
         ##postgresql://<nombre_usuario>:<password>@<host>:<puerto>/<nombre_basededatos>
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:testing@localhost:5432/postgres'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://converter:0123456789@localhost:5432/postgres'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
         app.config['JWT_SECRET_KEY']='frase-secreta'
