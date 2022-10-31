c24687b23c2ca696501518d0124d088c5645013a
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-10-31 13:00:49 -0500

    create app import

--- converter/views/views.py
+++ converter/views/views.py
@@ -54,12 +54,12 @@ class ViewConverter(Resource):
             db.session.commit()
             print('new_file')
 
-            """ task.output_file_id = new_file.id
+            task.output_file_id = new_file.id
             print('task')
             task.status = ProcessStatus.PROCESSED
             
             db.session.commit()
-            print('commit')"""
+            print('commit')
             return {"mensaje": "Se ha convertido el archivo con exito", "error": False}
         except :
             return {"mensaje": "Hubo un error no esperado", "error": True}
