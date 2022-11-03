40ff9cecf924fc2dd6ba1deb692a7cdbd6ed2462
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-11-03 09:56:55 -0500

    path_root converter

--- converter/configuration.py
+++ converter/configuration.py
@@ -10,6 +10,7 @@ class BaseConfig(object):
 class GCPConfig(BaseConfig):
     PATH_FILES='/home/'
     SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:0123456789@35.238.2.63:5432/postgres'
+    PATH_ROOT="/home/j_albarracinp/cloud-audio-converter/converter"
 
 class DevJhonConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI= 'postgresql://converter:0123456789@localhost:5432/postgres'
